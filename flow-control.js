bc398292061ce7cf65b74478b7ff32cac071c7dd
Brendan M <bmcginnnn@gmail.com>
2019-04-17 15:47:20 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -3,9 +3,6 @@ function basicTeenager(age) {
     console.log("You are a teenager!");
 }
 
-console.log(basicTeenager(13));
-console.log(basicTeenager(12));
-
 function teenager(age) {
   if (age >= 13 && age <= 19) {
     "You are a teenager!";
@@ -13,9 +10,6 @@ function teenager(age) {
     "You are not a teenager";
   }
 }
-
-console.log(teenager(13));
-console.log(teenager(12));
 
 function ageChecker(age) {
   if (age >= 13 && age <= 19) {
@@ -26,10 +20,6 @@ function ageChecker(age) {
     "You are a grownup";
   }
 }
-
-console.log(ageChecker(13));
-console.log(ageChecker(12));
-console.log(ageChecker(20));
 
 function ternaryTeenager(age) {
 
